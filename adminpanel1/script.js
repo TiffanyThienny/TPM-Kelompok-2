e416f0f17c8db7200c665a214fb8fd4898b5dae6
AndreasEdo <andreasedwardputrajatmiko@gmail.com>
2025-01-30 09:47:02 +0700

    Login Page

--- adminpanel1/script.js
+++ adminpanel1/script.js
@@ -1,5 +1,23 @@
-const hamBurger = document.querySelector(".toggle-btn");
+const rows = document.querySelectorAll('.rowcounter'); 
+const rowcount = rows.length;
 
-hamBurger.addEventListener("click", function () {
+const teamcount = document.getElementById('teamcount');
+teamcount.innerHTML = rowcount;
+
+
+
+const general = document.querySelector(".toggle-btn");
+
+general.addEventListener("click", function () {
   document.querySelector("#sidebar").classList.toggle("expand");
+});
+
+const masterCheckbox = document.getElementById('masterCheckbox');
+
+
+masterCheckbox.addEventListener('change', function() {
+    const checkboxes = document.querySelectorAll('.checkboxchild input'); 
+    checkboxes.forEach(checkbox => {
+        checkbox.checked = masterCheckbox.checked; 
+    });
 });
